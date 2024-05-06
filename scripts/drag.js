a5812a1eacbff3a793afcc83356ccfb2e4769f3b
Bamigreat <164428472+Bamigreat@users.noreply.github.com>
2024-05-06 16:41:42 +0100

    Add files via upload

---
+++ scripts/drag.js
@@ -0,0 +1,19 @@
+const carousel = document.querySelector(".carousel");
+
+let isDragging = false;
+
+const dragStart = () => {
+  isDragging = true;
+  carousel.classList.add("dragging");
+}
+
+const dragging = (e) => {
+  if(!isDragging) return;
+ carousel.scrollLeft = e.pageX;
+
+ console.log(e.pageX)
+} 
+
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
